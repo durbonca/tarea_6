8d6edc708c50db99bfd6ab677f682cb7ec93b0cb
durbonca <durbonca@gmail.com>
2022-11-26 12:41:39 -0300

    presentando a los monos esos

--- src/App.js
+++ src/App.js
@@ -32,8 +32,10 @@ export const App = () => {
 
   return (
     <div>
-        <h1>Pokemonos</h1>
-        <div>
+        <h1 className="mt-4 text-center text-3xl font-bold underline">Pokemonos</h1>
+        <div 
+            className="m-4 p-6 rounded-lg border-solid border-8 border-zinc-900 flex flex-wrap justify-center gap-4 bg-red-400"
+        >
             {pokemonList?.map((pokemon) => (
                 <Pokemono key={pokemon.id} pokemon={pokemon} />
             ))}
--- src/Pokemono.jsx
+++ src/Pokemono.jsx
@@ -1,9 +1,11 @@
 export const Pokemono = ({ pokemon }) => {
     const { name, imgUrl } = pokemon;
   return (
-    <div>
-        <img src={imgUrl} alt={name} />
-        {name}
+    <div className="bg-blue-100 p-4 rounded-md" >
+        <img width="100%" src={imgUrl} alt={name} />
+        <div className="text-center text-lg">
+          {name}
+        </div>
     </div>
   )
 }
